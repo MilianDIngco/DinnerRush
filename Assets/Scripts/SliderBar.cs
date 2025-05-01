f7482b2372b297660768c0aceb69a43b4ebf1077
Princess Manzano <princess@Princesss-MacBook-Air.local>
2025-05-01 09:45:46 -0400

    Added landing page

--- Assets/Scripts/SliderBar.cs
+++ Assets/Scripts/SliderBar.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SliderBar : MonoBehaviour
 {
     public Slider slider;
+    public TextMeshProUGUI gameOver;
     public void SetSlider(float value){
         slider.value = value;
+        checkMax();
+    }
+
+    void checkMax(){
+        if (slider.value >= 100){
+            gameOver.enabled = true;
+        }
     }
 }
